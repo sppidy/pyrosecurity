9e2c09418b1bdfd10461a53268df84d5026219a7
ѕρι∂у <rshouriesh@gmail.com>
2020-10-27 09:44:55 +0530

    Updated Strings

--- pyrobot/plugins/restrictions/restrict.py
+++ pyrobot/plugins/restrictions/restrict.py
@@ -34,15 +34,15 @@ async def mute_user(_, message):
             await message.reply_text(
                 "👍🏻 "
                 f"{user_first_name}"
-                " ലവന്റെ വായടച്ചിട്ടുണ്ട്! 🤐"
+                " Shh Be Quiet Now !! 🤐"
             )
         else:
             await message.reply_text(
                 "👍🏻 "
                 f"<a href='tg://user?id={user_id}'>"
-                "ലവന്റെ"
+                "His"
                 "</a>"
-                " വായടച്ചിട്ടുണ്ട്! 🤐"
+                " Mouth is Zipped Now Lol !! 🤐"
             )
 
 
@@ -61,8 +61,8 @@ async def temp_mute_user(_, message):
     if until_date_val is None:
         await message.reply_text(
             (
-                "അസാധുവായ സമയ തരം വ്യക്തമാക്കി. "
-                "പ്രതീക്ഷിച്ചതു m, h, or d, കിട്ടിയത്: {}"
+                "Invalid time type specified. "
+                "Expected m, h, or d, obtained: {}"
             ).format(
                 message.command[1][-1]
             )
@@ -83,16 +83,16 @@ async def temp_mute_user(_, message):
     else:
         if str(user_id).lower().startswith("@"):
             await message.reply_text(
-                "കുറച്ചുനേരം മിണ്ടാതിരിക്ക്! 😠"
+                "Lol Stay for Sometime 😠"
                 f"{user_first_name}"
                 f" muted for {message.command[1]}!"
             )
         else:
             await message.reply_text(
-                "കുറച്ചുനേരം മിണ്ടാതിരിക്ക്! 😠"
+                "Be quiet for a while! 😠"
                 f"<a href='tg://user?id={user_id}'>"
-                "ലവന്റെ"
+                "He"
                 "</a>"
-                " വായ "
-                f" muted for {message.command[1]}!"
+                " is  "
+                f" Muted For {message.command[1]}!"
             )
